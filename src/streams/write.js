af7dddb363851c83be9266bd8ab639cdd98090d7
iliatech <iadomyshev@gmail.com>
2024-01-23 16:22:19 +0100

    feat: streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import { generatePath, getCurrentDirPath } from "../utils/index.js";
+import { createWriteStream } from "fs";
+
 const write = async () => {
-    // Write your code here 
+  const dirPath = getCurrentDirPath(import.meta.url);
+
+  const filePath = generatePath(dirPath, "files/fileToWrite.txt");
+
+  const writableStream = createWriteStream(filePath);
+
+  process.stdin.pipe(writableStream);
 };
 
 await write();
