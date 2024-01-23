f59d9ed17517c7fd6c5ea3d0a348d2fe1f4bac53
iliatech <iadomyshev@gmail.com>
2024-01-23 01:31:25 +0100

    feat: cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const resultArray = [];
+
+  args.forEach((arg, index) => {
+    if (arg.startsWith("--")) {
+      resultArray.push(`${arg.substring(2, arg.length)} is ${args[index + 1]}`);
+    }
+  });
+
+  console.log(resultArray.join(", "));
 };
 
 parseArgs();
