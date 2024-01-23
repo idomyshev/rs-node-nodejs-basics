0f3c1253a6e4b4422e2f4d6bcb14c2142f71ed90
iliatech <iadomyshev@gmail.com>
2024-01-23 09:28:00 +0100

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { readFile } from "fs/promises";
+import crypto from "crypto";
+import { generatePath, getCurrentDirPath } from "../utils/index.js";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const dirPath = getCurrentDirPath(import.meta.url);
+
+  const filePath = generatePath(dirPath, "files/fileToCalculateHashFor.txt");
+
+  const data = await readFile(filePath);
+
+  const hashHex = crypto.createHash("sha256").update(data).digest("hex");
+
+  process.stdout.write(`${hashHex}\n`);
 };
 
 await calculateHash();
