712322f0af0aacfddb3a34f6cb602c257f1d1e75
iliatech <iadomyshev@gmail.com>
2024-01-23 11:45:18 +0100

    feat: zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import zlib from "zlib";
+import { createReadStream, createWriteStream } from "fs";
+import { generatePath, getCurrentDirPath } from "../utils/index.js";
+
 const decompress = async () => {
-    // Write your code here 
+  const dirPath = getCurrentDirPath(import.meta.url);
+
+  const sourceFilePath = generatePath(dirPath, "files/archive.gz");
+  const targetFilePath = generatePath(dirPath, "files/fileToCompress.txt");
+
+  const readableStream = createReadStream(sourceFilePath);
+
+  const gunzipStream = zlib.createGunzip();
+
+  const writableStream = createWriteStream(targetFilePath);
+
+  readableStream.pipe(gunzipStream).pipe(writableStream);
 };
 
 await decompress();
